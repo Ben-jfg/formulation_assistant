41889a8d9d5e68e4563c8da1db866020d1977022
benf <benfr@navidia.com>
2022-03-20 09:50:34 +0200

    deployment

---
+++ formulation_assistant.py
@@ -0,0 +1,83 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from formulation_assistant_utils import *
+import time
+
+title_con = st.container()
+text_con = st.container()
+inputs_con = st.container()
+results_con = st.container()
+button_con = st.container()
+about_con = st.container()
+
+
+# with open('style.css') as f:
+#     st.markdown(f'<style>{f.read()}<style>', unsafe_allow_html=True)
+
+df_drug, df_cancer, dict_drug = get_data()
+drug_list, cancer_list = get_select_box_options(df_drug, df_cancer)
+with title_con:
+    st.title('Formulation Assistant')
+
+with text_con:
+    st.markdown('**Drug classifaction by Type as function of  nano-particals (NPs) stability**<br /> '
+                '<b><i><span style="color:#2B78FF">_Type 1_</b></i> - Drugs that formulate “Good” and stable NPs<br />'
+                '<b><i><span style="color:#2B78FF">_Type 2_</b></i> - Drugs that form Good but not stable NPs<br />'
+                '<b><i><span style="color:#2B78FF">_Type 3_</b></i>  - Aggregates<br />'
+                '<b><i><span style="color:#2B78FF">_Type 4_</b></i>  - Drugs that failed to precipitate<br />'
+                '\u2022 Particles are “Good” if size<150nm, PDI<  0.2,<br />'
+                '\u2022  Particles are stable if they remain “good” after 3 days', unsafe_allow_html=True)
+
+with button_con:
+    button_col1, button_col2, button_col3 = st.columns(3)
+
+    clear_button = button_col1.button('Clear')
+    if clear_button:
+        st.session_state['drug_in'] = []
+        st.session_state['cancer_in'] = ''
+
+    help_button = button_col3.button('Help')
+    if help_button:
+        st.markdown('_Help is on the way..._')
+
+    with inputs_con:
+        drug_in = st.multiselect('Select Drugs (optional, up to 2)', drug_list, key='drug_in')
+        cancer_in = st.selectbox('Select Cancer Type (optional)', cancer_list, key='cancer_in')
+
+with results_con:
+    if len(drug_in) > 2:
+        st.markdown('<b><span style="color:#AF1F22">Please select up to tow drugs', unsafe_allow_html=True)
+
+    elif len(drug_in) == 2:
+        if cancer_in == '':
+            df_results = search_by_two_drugs(drug_in, df_cancer)
+        else:
+            df_results = search_by_two_drugs_and_cancer(drug_in, cancer_in, df_cancer)
+
+    elif len(drug_in) == 1:
+        if cancer_in == '':
+            df_results = search_by_single_drug(drug_in[0], df_cancer)
+        else:
+            df_results = search_by_single_drug_and_cancer(drug_in[0], cancer_in, df_cancer)
+
+    elif len(drug_in) == 0:
+        if cancer_in == '':
+            df_results = None
+        else:
+            df_results = search_by_cancer(cancer_in, df_cancer)
+
+    plot_result_df(df_results, results_con, drug_in, dict_drug)
+
+with button_con:
+    csv_to_save = get_data_to_save(df_results)
+
+    export_button = button_col2.download_button(
+        label="Export",
+        data=csv_to_save,
+        file_name=f'Formulation_Assistant_Data_{time.strftime("%d.%m.%Y-%H:%M:%S")}.csv',
+        mime='text/csv',
+    )
+
+with about_con:
+    st.markdown('\xA9 Shamay\'s Lab')
---
+++ formulation_assistant_utils.py
@@ -0,0 +1,183 @@
+import pandas as pd
+import streamlit as st
+import plotly.graph_objects as go
+
+
+@st.cache
+def get_data():
+    xls = pd.ExcelFile("Drug Classification and synergy2.0.xlsx")
+    df_drug = pd.read_excel(xls, 'Types(chemistry)')
+    df_cancer = pd.read_excel(xls, 'Synergy(biology)')
+    dict_drug = {}
+    for cur_col in list(df_drug):
+        for cur_drug in list(df_drug[cur_col].dropna()):
+            if cur_drug.isupper():
+                dict_drug[cur_drug] = cur_col
+            else:
+                dict_drug[cur_drug.title()] = cur_col
+
+    pd.options.mode.chained_assignment = None
+    all_drug_type = []
+    confidence_list = []
+    for index, row in df_cancer.iterrows():
+        if not row['Cancers'].isupper():
+            df_cancer['Cancers'][index] = row['Cancers'].title()
+        if not row['TypeI'].isupper():
+            df_cancer['TypeI'][index] = row['TypeI'].title()
+        if not row['All drugs'].isupper():
+            df_cancer['All drugs'][index] = row['All drugs'].title()
+
+        if row['All drugs'] in list(dict_drug.keys()):
+            all_drug_type.append(dict_drug[row['All drugs']])
+        else:
+            all_drug_type.append('Type NA')
+
+        confidence_list.append(set_confidence(row))
+
+    df_cancer['all_drug_type'] = all_drug_type
+    df_cancer['confidence_level'] = confidence_list
+    return df_drug, df_cancer, dict_drug
+
+
+@st.cache
+def get_select_box_options(df_drug, df_cancer):
+    drug_list = []
+    for cur_type in list(df_drug):
+        drug_list = drug_list + df_drug[cur_type].dropna().to_list()
+    drug_list = list(set(drug_list))
+    # drug_list.append('')
+    drug_list.sort(reverse=False)
+
+    cancer_list = list(set(df_cancer['Cancers'].to_list()))
+    cancer_list.append('')
+    cancer_list.sort(reverse=False)
+    return drug_list, cancer_list
+
+
+def set_confidence(row, confidence_threshold=None):  # 1-3 low , 4-10 medium, >11 high
+    if confidence_threshold is None:
+        confidence_threshold = [3, 10]
+    if row['# of publications'] <= confidence_threshold[0]:
+        return 'low'
+    elif row['# of publications'] <= confidence_threshold[1]:
+        return 'medium'
+    elif row['# of publications'] > confidence_threshold[1]:
+        return 'high'
+    else:
+        assert 'bad # of publications'
+
+
+def get_bold_headers(results_df):
+    bold_list = []
+    for col in list(results_df):
+        bold_list.append('<b>' + col)
+    return bold_list
+
+
+def plot_result_df(results_df, results_filed, drugs, dict_drug):
+    if results_df is None:
+        results_filed.markdown('_The results will appear here..._')
+        return
+
+    if not results_df.empty:
+        fig = go.Figure(data=go.Table(
+            header=dict(values=get_bold_headers(results_df),
+                        fill_color='#A5C2FF',
+                        align='center'),
+            cells=dict(values=results_df.transpose().values.tolist(),
+                       fill_color='#F0F2F6',
+                       align='left')))
+        fig.update_layout(margin=dict(l=0, r=0, b=0, t=0), width=135*len(list(results_df)), height=1000)
+        results_filed.write(fig)
+
+    elif results_df.empty:
+        no_results_str = ''
+        valid = 0
+        for drug in drugs:
+            no_results_str += f"<b>{drug} is a {dict_drug[drug]} drug</b><br>"
+            if len(drugs) == 2 and dict_drug[drug] == 'Type 1':
+                valid = 1
+        if len(drugs) == 2 and valid:
+            no_results_str += f'<b><span style="color:#2B78FF">{drugs[0]} and {drugs[1]} _can_ be combined to create stable NPs</b><br>'
+            no_results_str += f'<br><b>No additional information was found</b><br>'
+        elif len(drugs) == 2 and not valid:
+            no_results_str += f'<b><span style="color:#AF1F22">{drugs[0]} and {drugs[1]} _cannot_ be combined to create stable NPs</b><br>'
+            no_results_str += f'<br><b>No additional information was found</b><br>'
+        else:
+            no_results_str += f'<br><b><span style="color:#AF1F22">No results were found, please try to relax the constraints'
+
+        results_filed.markdown(f'{no_results_str}', unsafe_allow_html=True)
+
+
+def get_data_to_save(results_df):
+    temp = pd.DataFrame()
+    if results_df is None or results_df.empty:
+        return temp.to_csv(index=False).encode('utf-8')
+    else:
+        return results_df.to_csv(index=False).encode('utf-8')
+
+
+## Search functions:
+def init_result_dict():
+    return {'Drug I (type 1)': [], 'Drug II': [], 'Drug II type': [], 'Cancer Type': [], 'Confidence Level': []}
+
+
+def add_to_result_dict(row, result_dict):
+    result_dict['Drug I (type 1)'].append(row["TypeI"])
+    result_dict['Drug II'].append(row["All drugs"])
+    result_dict['Drug II type'].append(row["all_drug_type"])
+    result_dict['Cancer Type'].append(row["Cancers"])
+    result_dict['Confidence Level'].append(row["confidence_level"])
+    return result_dict
+
+
+def get_result_df(result_dict):
+    result_df = pd.DataFrame.from_dict(result_dict)
+    # st.write(result_df)
+    # st.write(result_dict)
+    result_df = result_df.sort_values(by=['Drug I (type 1)', 'Drug II', 'Cancer Type'])
+    return result_df
+
+
+def search_by_cancer(cancer, df_cancer):
+    result_dict = init_result_dict()
+    for index, row in df_cancer.iterrows():
+        if row['Cancers'] == cancer:
+            result_dict = add_to_result_dict(row, result_dict)
+
+    return get_result_df(result_dict)
+
+
+def search_by_single_drug(drug, df_cancer):
+    result_dict = init_result_dict()
+    for index, row in df_cancer.iterrows():
+        if row["TypeI"] == drug or row["All drugs"] == drug:
+            if row["TypeI"] == row["All drugs"]:
+                continue
+            result_dict = add_to_result_dict(row, result_dict)
+    return get_result_df(result_dict)
+
+
+def search_by_two_drugs(drugs, df_cancer):
+    result_dict = init_result_dict()
+    for index, row in df_cancer.iterrows():
+        if (row["TypeI"] == drugs[0] and row["All drugs"] == drugs[1]) or (row["TypeI"] == drugs[1] and row["All drugs"] == drugs[0]):
+            result_dict = add_to_result_dict(row, result_dict)
+    return get_result_df(result_dict)
+
+
+def search_by_single_drug_and_cancer(drug, cancer, df_cancer):
+    result_dict = init_result_dict()
+    for index, row in df_cancer.iterrows():
+        if (row["TypeI"] == drug or row["All drugs"] == drug) and (row['Cancers'] == cancer):
+            result_dict = add_to_result_dict(row, result_dict)
+    return get_result_df(result_dict)
+
+
+def search_by_two_drugs_and_cancer(drugs, cancer, df_cancer):
+    result_dict = init_result_dict()
+    for index, row in df_cancer.iterrows():
+        if (row["TypeI"] == drugs[0] and row["All drugs"] == drugs[1]) or (row["TypeI"] == drugs[1] and row["All drugs"] == drugs[0]):
+            if row['Cancers'] == cancer:
+                result_dict = add_to_result_dict(row, result_dict)
+    return get_result_df(result_dict)
